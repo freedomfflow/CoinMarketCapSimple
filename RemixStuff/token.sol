e8bde21b4a2f8fb4fc5bcf1dd4a5cc7d33cd4e6f
Gary Wolff <wolff@freedomfflow.com>
2020-09-05 14:42:30 -0400

    Adding gitignore

---
+++ RemixStuff/token.sol
@@ -0,0 +1,71 @@
+pragma solidity 0.5.12;
+
+import "./Ownable.sol";
+
+contract ERC20 is Ownable {
+
+    mapping (address => uint256) private _balances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor (string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = 18;
+    }
+
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        require(account != address(0), "Not allowed to mint to 0 address");
+        _totalSupply += amount;
+        _balances[account] += amount;
+        return;
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        // Check balance
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance for requested transfer");
+
+        // Save pre-txn balances
+        uint256 initialSenderBalance = balanceOf[msg.sender];
+        uint256 initialRecipientBalance = balanceOf[recipient];
+
+        // Update _balances
+        uint xfrAmount = amount;
+        _balances[msg.sender] -= xfrAmount;
+        _balances[recipient] += xfrAmount;
+
+        // Transfer tokens
+        msg.sender.transfer(xfrAmount);
+
+        // Assert new balances are updated accurately
+        assert(balanceOf[recipient] == initialRecipientBalance + xfrAmount);
+        assert(balanceOf[msg.sender] == initialSenderBalance - xfrAmount);
+
+        return true;
+    }
+}
